d004df99f66999684553da8b84d3cadd1f40280b
Aojia Zhao <aojia93@stanford.edu>
2013-02-21 21:37:45 -0800

    committing for multiplechoicechoices

--- hw6/src/site/MultipleChoiceChoices.java
+++ hw6/src/site/MultipleChoiceChoices.java
@@ -1,5 +1,17 @@
 package site;
 
 public class MultipleChoiceChoices {
+	private String choiceString;
+	
+	public MultipleChoiceChoices(String choiceString) {
+		this.setChoiceString(choiceString);
+	}
 
+	public String getChoiceString() {
+		return choiceString;
+	}
+
+	public void setChoiceString(String choiceString) {
+		this.choiceString = choiceString;
+	}
 }
