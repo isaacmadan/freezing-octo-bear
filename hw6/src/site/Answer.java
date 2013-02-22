8887f9dabdb248432a3c50824a6b1f5286739a7e
Aojia Zhao <aojia93@stanford.edu>
2013-02-21 21:55:37 -0800

    done with quiz and answer class

--- hw6/src/site/Answer.java
+++ hw6/src/site/Answer.java
@@ -1,5 +1,33 @@
 package site;
 
+import java.util.HashSet;
+
 public class Answer {
 
+	private HashSet<String> answers;
+	
+	public Answer(HashSet<String> answers) {
+		this.setAnswers(answers);
+	}
+	
+	public boolean contains(String response) {
+		if(answers.contains(response)) return true;
+		return false;
+	}
+
+	public void addAnswer(String str) {
+		answers.add(str);
+	}
+	
+	public void removeAnswer(String str) {
+		answers.remove(str);
+	}
+	
+	public HashSet<String> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(HashSet<String> answers) {
+		this.answers = answers;
+	}
 }
--- hw6/src/site/Quiz.java
+++ hw6/src/site/Quiz.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class Quiz {
 	
-	ArrayList<Question> questions;
-	int quiz_id;
-	int max_score;
+	private ArrayList<Question> questions;
+	private int quiz_id;
+	private int max_score;
 	
-	public Quiz() {
-		this.questions = new ArrayList<Question>();
+	public Quiz(ArrayList<Question> questions) {
+		this.setQuestions(questions);
 	}
 	
 	public void addQuestion(Question question) {
@@ -18,5 +18,25 @@ public class Quiz {
 	
 	public ArrayList<Question> getQuestions() {
 		return questions;
+	}
+	
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+
+	public int getQuiz_id() {
+		return quiz_id;
+	}
+
+	public void setQuiz_id(int quiz_id) {
+		this.quiz_id = quiz_id;
+	}
+
+	public int getMax_score() {
+		return max_score;
+	}
+
+	public void setMax_score(int max_score) {
+		this.max_score = max_score;
 	}
 }
