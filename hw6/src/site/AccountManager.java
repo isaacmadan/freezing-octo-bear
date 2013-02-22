70efdf56ece9a1cc3318f0ac961af9d3db90d8a0
Isaac Madan <isaacmadan@gmail.com>
2013-02-21 20:03:05 -0800

    worked on login basics

---
+++ hw6/src/site/AccountManager.java
@@ -0,0 +1,25 @@
+package site;
+
+
+public class AccountManager {
+
+	
+	public AccountManager() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isExistingAccount() { return false; }
+	
+	public boolean isPasswordCorrect() { return false; }
+	
+	public void createNewAccount() {}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- hw6/src/site/User.java
+++ hw6/src/site/User.java
@@ -1,14 +1,67 @@
 package site;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class User {
 
-	public User() {
-		// TODO Auto-generated constructor stub
+	private int id;
+	private String username;
+	private String password;
+	//Friends
+	//Inbox
+	//History
+	//Achievements
+	private boolean isAdmin;
+	
+	public User(String username, String password, boolean isAdmin) {
+		
+		this.username = username;
+		this.password = password;
+		
+		this.isAdmin = isAdmin;
+		
+	}
+	
+	/*
+	 Given a byte[] array, produces a hex String,
+	 such as "234a6f". with 2 chars for each byte in the array.
+	 (provided code)
+	*/
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
+	
+	public String hashPassword(String input) {
+		byte[] bytes = input.getBytes();
+		
+		MessageDigest hash = null; //SHA hash
+		try {
+			hash = MessageDigest.getInstance("SHA");
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+		}
+		
+		String res = null;
+		try {
+			hash.update(bytes);
+			byte[] output = hash.digest();
+			res = hexToString(output);
+		}
+		catch(Exception e) {
+			
+		}
+		
+		return res;
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
