bd509693f4b7d1fcc4c026cbd1917d8b9bd03806
Isaac Madan <isaacmadan@gmail.com>
2013-02-21 19:33:27 -0800

    added some starter files to test

---
+++ hw6/src/site/User.java
@@ -0,0 +1,17 @@
+package site;
+
+public class User {
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
