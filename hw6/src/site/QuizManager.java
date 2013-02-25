16f1db7e6da152a9eac969a1bee2de422545b5da
Aojia Zhao <aojia93@stanford.edu>
2013-02-24 20:57:07 -0800

    update for isaac testing

--- hw6/src/site/QuizManager.java
+++ hw6/src/site/QuizManager.java
@@ -37,16 +37,41 @@ public class QuizManager {
 	
 	public void addQuizToDataBase() {
 		try {
-			Statement stmt = con.createStatement(); //construct search query based on inputs
+			Statement stmt = con.createStatement();
 			stmt.executeQuery("INSERT INTO quizzes (user_id, practice_mode, description, title, max_score," +
 					"random_question, one_page, immediate_correction) VALUES(" + user_id + "," + practice_mode + "," +
 					description + "," + title + "," + max_score + "," + random_question + "," + one_page + "," + immediate_correction + ");");
+			if(request.getParameter("question_response_count") != null) addQuestionResponseToDataBase();
+			if(request.getParameter("fill_in_the_blank_count") != null) addFillInTheBlankToDataBase();
+			if(request.getParameter("multiple_choice_count") != null) addMultipleChoiceToDataBase();
+			if(request.getParameter("picture_response_count") != null) addPictureResponseToDataBase();
 		}
-		catch(Exception e) {}
+		catch(Exception e) { }
 	}
 	
 	private void addQuestionResponseToDataBase() {
+		try {
+			Statement stmt = con.createStatement();
+			for(int i = 0; i < Integer.parseInt(request.getParameter("question_response_count")); i++) {
+				String addingToQuestionDB = "INSERT INTO questions (quiz_id, point_value, question_type)"
+						+ "VALUES(" + ;
+			}
+			stmt.executeQuery("INSERT INTO quizzes (user_id, practice_mode, description, title, max_score," +
+					"random_question, one_page, immediate_correction) VALUES(" + user_id + "," + practice_mode + "," +
+					description + "," + title + "," + max_score + "," + random_question + "," + one_page + "," + immediate_correction + ");");
+		} catch(Exception e) { }
+	}
+	
+	private void addFillInTheBlankToDataBase() {
 		
+	}
+
+	private void addMultipleChoiceToDataBase() {
+	
+	}
+
+	private void addPictureResponseToDataBase() {
+	
 	}
 	
 }
